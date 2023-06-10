b6da2660dfac54aeb605a2743dafc59d86b73756
BelenMadariaga <mbmadariaga@uc.cl>
2023-06-10 14:29:43 -0400

    Add router

---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Layout = () => (
+  <Wrapper>
+    Layout
+  </Wrapper>
+)
+
+const Wrapper = styled.div``
+
+export default Layout
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const NotFound = () => (
+  <Wrapper>
+    Página no encontrada 💥
+  </Wrapper>
+)
+
+const Wrapper = styled.div``
+
+export default NotFound
