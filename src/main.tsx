d6d5d7c9bdfce30cd8ea2c95e6d5e2a6578666a5
BelenMadariaga <mbmadariaga@uc.cl>
2023-06-11 01:57:54 -0400

    Add constants

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './config/Router.tsx'
-import './index.css'
+import GlobalStyle from './styles/GlobalStyles.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <Router />
   </React.StrictMode>
 )
