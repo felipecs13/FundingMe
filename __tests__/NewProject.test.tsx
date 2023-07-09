6c31ab3148368da8bd25c627a44cd3bbf2968fd1
Felipe Carrasco <felipecs13@uc.cl>
2023-07-09 11:37:08 -0400

    try to pass test

--- __tests__/NewProject.test.tsx
+++ __tests__/NewProject.test.tsx
@@ -1,5 +1,8 @@
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import FormProject from '../src/views/NewProject';
+import React from 'react';
+import moment from 'moment';
+
 
 jest.mock('../src/assets/logo_green.png', () => 'logo_green.png');
 jest.mock('../src/assets/social_project.jpg', () => 'social_project.jpg');
@@ -20,12 +23,28 @@ describe('FormProject', () => {
           }))
         });
     });
+    beforeEach(() => {
+      // Mock the user state
+      jest.spyOn(React, 'useState').mockReturnValue([
+        { token: 'mockToken', id: 1, email: 'test@example.com', rut: '123456789', name: 'John Doe', is_admin: false },
+        jest.fn(), // Mock setter function
+      ]);
+
+  // Set the locale within the moment instance
+  moment.updateLocale('en', {});
+
+  // Set the mock current date and time
+  const now = new Date('2022-01-01T00:00:00.000Z');
+  jest.spyOn(Date, 'now').mockImplementation(() => now.getTime());
+});
+
+    
   it('renders without crashing', () => {
     render(<FormProject />);
   });
-
+  
   it('submits the form correctly', async () => {
-    
+
     const { getByLabelText, getByText } = render(<FormProject />);
 
     fireEvent.change(getByLabelText('Nombre del proyecto'), { target: { value: 'Test Project' } });
@@ -35,7 +54,7 @@ describe('FormProject', () => {
     fireEvent.change(getByLabelText('Tipo de proyecto'), { target: { value: 'ONG' } });
     fireEvent.change(getByLabelText('Categoría'), { target: { value: 'EDUCATION' } });
     fireEvent.change(getByLabelText('Locación'), { target: { value: 'Test Location' } });
-    fireEvent.change(getByLabelText('Fecha de término'), { target: { value: '2022-01-01' } });
+    fireEvent.change(getByLabelText('Fecha de término'), { target: { value: '12-12-2024' } });
     fireEvent.change(getByLabelText('Monto mínimo de donación'), { target: { value: '500' } });
 
     fireEvent.click(getByText('Enviar proyecto'));
